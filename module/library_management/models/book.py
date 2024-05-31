a827e308b0578d842a2f822c09db03d809e6e010
Carlos Atanes Vences <carlos.atanes@cesumin.com>
2024-05-31 11:19:05 +0200

    wip: library_management functionality

--- module/library_management/models/book.py
+++ module/library_management/models/book.py
@@ -1,20 +1,44 @@
 # library_management/models/book.py
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 class LibraryBook(models.Model):
     _name = 'library.book'
     _description = 'Library Book'
 
-    # Campo para el título del libro
     name = fields.Char(string='Title', required=True)
-    
-    # Campo para el autor del libro
     author = fields.Char(string='Author', required=True)
-    
-    # Campo para la fecha de publicación del libro
     published_date = fields.Date(string='Published Date')
-    
-    # Campo para la descripción del libro
     description = fields.Text(string='Description')
+
+    @api.model
+    def create_book(self, name, author, published_date, description):
+
+        # Crear un nuevo libro
+        book = self.create({
+            'name': name,
+            'author': author,
+            'published_date': published_date,
+            'description': description,
+        })
+        return book
+    
+    @api.model
+    def update_book(self, book_id, name, author, published_date, description):
+        
+        # Actualizar un libro
+        book = self.browse(book_id)
+        book.name = name
+        book.author = author
+        book.published_date = published_date
+        book.description = description
+        return book
+    
+    @api.model
+    def delete_book(self, book_id):
+
+        # Eliminar un libro
+        book = self.browse(book_id)
+        book.unlink()
+        return True
